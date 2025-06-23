fb3777eccda6201287663bf50f6bf4fb14b9ab10
Nosa Onaiwu <officiallyprime@gmail.com>
2025-06-23 16:50:55 +0100

    Delete src directory

--- src/utils/request.utils.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from "express";
-
-//Extending the Request interface with the user who made the request
-export interface IUserRequest extends Request {
-  user?: { userId: string };
-}
