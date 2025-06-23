cbbbc0fe4edd07ae8253f45ad783cb20fdd41360
Nosa Onaiwu <officiallyprime@gmail.com>
2025-06-23 16:50:41 +0100

    Delete config directory

--- config/default.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-
-export default {
-  port: process.env.PORT as unknown as number,
-  //dbUrl: process.env.DB_URL as string,
-  saltRounds: 11 as number,
-  jwtSecretKey: process.env.JWT_SECRET_KEY as string,
-};
